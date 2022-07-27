2c7e2521edbc68ffc3036f8f4711995665694111
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-07-27 19:57:28 +0530

    fix - icon fill

--- src/Assets/Icons/UserIcon.tsx
+++ src/Assets/Icons/UserIcon.tsx
@@ -7,7 +7,7 @@ export const UserIcon: ReactSVGIcon = (props) => (
         viewBox="0 0 24 24"
         {...props}>
         <path
-            fill="currentColor"
+            fill={props.fill}
             d="M4 22a8 8 0 1 1 16 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6s6 2.685 6 6s-2.685 6-6 6z"></path>
     </svg>
 );
